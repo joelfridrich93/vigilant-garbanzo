e90f4d834fe9b568a823392dd0f502750a896091
Joel Fridrich <joelfridrich@gmail.com>
2018-02-26 08:42:36 +0100

    Update instructions and add viktors solution.

--- products_of_all_ints.php
+++ products_of_all_ints.php
@@ -1,15 +1,16 @@
 <?php
 
-// Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of integers and returns an array of the products.
+// Write a function that takes an array of integers and returns an array of the products except at the index.
 // For example, given:
 // [1, 7, 3, 4]
 // your function would return:
 // [84, 12, 28, 21]
 // by calculating:
 // [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
-//Do not use division in your solution.
+// Do not use division in your solution.
 
 /**
+ * Solution Joel
  * @param array $numbers
  * @return array
  */
@@ -26,3 +27,12 @@ function getProductsOfAllIntsExceptAtIndex(array $numbers): array
 }
 
 print_r(getProductsOfAllIntsExceptAtIndex([1, 7, 3, 4]));
+
+
+// Viktors 
+function getProductsOfAllIntsExceptAtIndex(array $input): array
+{
+    return array_map(function ($elem) use ($input) {
+        return array_product(array_diff($input, [$elem]));
+    }, $input);
+}
